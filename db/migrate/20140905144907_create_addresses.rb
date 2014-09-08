204997416b06d402acfc9a78518299ec0f04ec17
tmerlet <tmerlet@gmail.com>
2014-09-08 12:34:55 +0100

    cleaning up address

--- db/migrate/20140905144907_create_addresses.rb
+++ db/migrate/20140905144907_create_addresses.rb
@@ -1,11 +1,11 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
-      t.string :line1, :null => false
+      t.string :line1 
       t.string :line2
-      t.string :city, :null => false
-      t.string :postcode, :null => false, :limit => 55
-      t.string :country, :null => false
+      t.string :city 
+      t.string :postcode
+      t.string :country
 
       # for the polymorphic association
       t.integer :addressable_id
